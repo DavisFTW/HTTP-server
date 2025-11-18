c2df5a010270852f50b8fde6fc29181c9d0b1c48
Davis <davisrafaelsdainis@gmail.com>
2025-11-18 18:27:33 +0200

    small typo fix

--- server.h
+++ server.h
@@ -1,82 +1,82 @@
-        //
-        // Created by Davis on 10/29/2025.
-        //
+    //
+    // Created by Davis on 10/29/2025.
+    //
 
-        #ifndef UNTITLED_SERVER_H
-        #define UNTITLED_SERVER_H
+    #ifndef UNTITLED_SERVER_H
+    #define UNTITLED_SERVER_H
 
-        #include <iostream>
-        #include <winsock2.h>
-        #include <ws2tcpip.h>
-        #include <fstream>
-        #include <filesystem>
+    #include <iostream>
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <fstream>
+    #include <filesystem>
 
-        #include "parser.h"
+    #include "parser.h"
 
-        class socketRAII {
-        private:
-            SOCKET sock;
-        public:
-            socketRAII() : sock(INVALID_SOCKET) {}
+    class socketRAII {
+    private:
+        SOCKET sock;
+    public:
+        socketRAII() : sock(INVALID_SOCKET) {}
 
-            explicit socketRAII(SOCKET sock) : sock(sock) {}
-            ~socketRAII() {
+        explicit socketRAII(SOCKET sock) : sock(sock) {}
+        ~socketRAII() {
+            if (this->sock != INVALID_SOCKET) {
+                closesocket(this->sock);
+            }
+        }
+
+        // copy constructor
+        socketRAII(const socketRAII&) = delete;
+        //copy operator
+        socketRAII& operator=(const socketRAII&) = delete;
+
+        // move constructor
+        socketRAII(socketRAII&& other) noexcept : sock(other.sock) {
+            other.sock = INVALID_SOCKET; // prevents destructor
+        }
+        // mnove assigment
+        socketRAII& operator=(socketRAII&& other) noexcept {
+            // no self assingnes
+            if (this != &other) {
+                // Close MY current socket (if I have one)
                 if (this->sock != INVALID_SOCKET) {
                     closesocket(this->sock);
                 }
+                this->sock = other.sock;
+
+                // Set other's socket to INVALID_SOCKET so its destructor won't close it
+                other.sock = INVALID_SOCKET;
             }
-
-            // copy constructor
-            socketRAII(const socketRAII&) = delete;
-            //copy operator
-            socketRAII& operator=(const socketRAII&) = delete;
-
-            // move constructor
-            socketRAII(socketRAII&& other) noexcept : sock(other.sock) {
-                other.sock = INVALID_SOCKET; // prevents destructor
-            }
-            // mnove assigment
-            socketRAII& operator=(socketRAII&& other) noexcept {
-                // no self assingnes
-                if (this != &other) {
-                    // Close MY current socket (if I have one)
-                    if (this->sock != INVALID_SOCKET) {
-                        closesocket(this->sock);
-                    }
-                    this->sock = other.sock;
-
-                    // Set other's socket to INVALID_SOCKET so its destructor won't close it
-                    other.sock = INVALID_SOCKET;
-                }
-                return *this;
-            };
-
-            SOCKET get() const { return this->sock; };
-            bool validate() const;
+            return *this;
         };
 
+        SOCKET get() const { return this->sock; };
+        bool validate() const;
+    };
 
-        class server {
-        private:
-            const WORD wVersionRequested = MAKEWORD(2, 2);
 
-            const int port = 5051;
-            WSADATA wsaData;
-            socketRAII sock;
-            struct sockaddr_in  local_sin{};
+    class server {
+    private:
+        const WORD wVersionRequested = MAKEWORD(2, 2);
 
-            const int backlog = 5;
+        const int port = 5051;
+        WSADATA wsaData;
+        socketRAII sock;
+        struct sockaddr_in  local_sin{};
 
-            std::filesystem::path projectPath;
+        const int backlog = 5;
 
-            std::string buildHttpResponse(int bodyLength);
+        std::filesystem::path projectPath;
 
-        public:
+        std::string buildHttpResponse(int bodyLength);
 
-            server(const std::filesystem::path& projectPath);
-            int init();
-            int start();
-            int cleanup();
+    public:
 
-        };
-        #endif //UNTITLED_SERVER_H
+        server(const std::filesystem::path& projectPath);
+        int init();
+        int start();
+        int cleanup();
+
+    };
+    #endif //UNTITLED_SERVER_H
