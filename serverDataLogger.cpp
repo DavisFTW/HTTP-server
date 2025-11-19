8f112f6aa7ebfc7693ee026728a08f293f9d1ce2
Davis <davisrafaelsdainis@gmail.com>
2025-11-19 13:39:28 +0200

    Added serverDataLogging.cpp

---
+++ serverDataLogger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Davis on 11/19/2025.
+//
+
+#include "serverDataLogger.h"
---
+++ serverDataLogger.h
@@ -0,0 +1,13 @@
+//
+// Created by Davis on 11/19/2025.
+//
+
+#ifndef UNTITLED_SERVERDATALOGGER_H
+#define UNTITLED_SERVERDATALOGGER_H
+
+
+class serverDataLogger {
+};
+
+
+#endif //UNTITLED_SERVERDATALOGGER_H
